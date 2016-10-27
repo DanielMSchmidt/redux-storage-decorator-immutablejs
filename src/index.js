4215c79d7c229ab930cec7457c93f352d5e9c8cb
Daniel Schmidt <danielmschmidt92@gmail.com>
2016-10-28 00:02:08 +0200

    removed whitelist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import { fromJS } from 'immutable';
 
-export default (engine, whitelist = []) => {
+export default engine => {
     return {
         ...engine,
 
         load() {
-            return engine.load().then((result) => {
-                whitelist.forEach((key) => {
-                    result[key] = fromJS(result[key]);
-                });
-                return result;
-            });
+            return engine.load().then(result => fromJS(result));
         },
     };
 };
